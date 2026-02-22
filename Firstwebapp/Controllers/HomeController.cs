bd13716ce2353a97c9776b5eb18612ea109064ba
saradev <saradacharya592@gmail.com>
2026-02-22 22:43:57 +0545

    Simple Dashboard is added, and a button for easier navigation

--- Firstwebapp/Controllers/HomeController.cs
+++ Firstwebapp/Controllers/HomeController.cs
@@ -22,6 +22,24 @@ public class HomeController : Controller
     {
         return View();
     }
+    public IActionResult Dashboard()
+    {
+        // Sample data for the dashboard
+        ViewBag.WorkerCount = 5;
+        ViewBag.ActiveJobs = 3;
+        ViewBag.PendingPayments = 2;
+        ViewBag.OutstandingAmount = 4250.00;
+    
+        // Sample list of today's jobs
+        var todaysJobs = new List<dynamic>
+        {
+            new { Customer = "Rabi khanal", Time = "8:00 AM", Worker = "Moturam", Status = "Pending" },
+            new { Customer = "Dawa ", Time = "2:00 PM", Worker = "Rajesh", Status = "In Progress" },
+            new { Customer = "Keshav Dhungana", Time = "4:30 PM", Worker = "Rajendra", Status = "Pending" }
+        };
+    
+        return View(todaysJobs);
+    }
 
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     public IActionResult Error()
